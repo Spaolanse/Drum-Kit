a2982d664cf6b7b61839cb91e736c1eb0b2d4799
Spaolanse <gustavospaolanse@gmail.com>
2024-06-02 01:43:27 -0300

    Update

--- script/index.js
+++ script/index.js
@@ -1,75 +1,50 @@
-document.querySelectorAll(".drum")[0].addEventListener("click", function(){
-    const audioTomOne = new Audio("sounds/tom-1.mp3");
-    audioTomOne.play();
-});
-document.querySelectorAll(".drum")[1].addEventListener("click", function(){
-    const audioTomTwo = new Audio("sounds/tom-2.mp3");
-    audioTomTwo.play();
-});
-document.querySelectorAll(".drum")[2].addEventListener("click", function(){
-    const audioTomThree = new Audio("sounds/tom-3.mp3");
-    audioTomThree.play();
-});
-document.querySelectorAll(".drum")[3].addEventListener("click", function(){
-    const audioTomFour = new Audio("sounds/tom-4.mp3");
-    audioTomFour.play();
-});
-document.querySelectorAll(".drum")[4].addEventListener("click", function(){
-    const audioSnare = new Audio("sounds/snare.mp3");
-    audioSnare.play();
-});
-document.querySelectorAll(".drum")[5].addEventListener("click", function(){
-    const audioCrash = new Audio("sounds/crash.mp3");
-    audioCrash.play();
-});
-document.querySelectorAll(".drum")[6].addEventListener("click", function(){
-    const audioKick = new Audio("sounds/kick-bass.mp3");
-    audioKick.play();
-});
+function animation(currentKey) {
+    const button = document.querySelector("." + currentKey);
+    button.classList.add("pressed");
+
+    setTimeout(function() {
+        button.classList.remove("pressed");
+    }, 50);
+}
+
+const drumLength = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < drumLength; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        const buttonClicked = this.innerHTML;
+        sound(buttonClicked);
+        animation(buttonClicked);
+    }); 
+};
 
 document.addEventListener("keypress", function(event){
-
-    Sound(event.key);
+    sound(event.key);
+    animation(event.key);
 });
 
-function Sound(key) {
-
-    switch(key){
-        case "w":
+function sound(key) {
+    if (key == "w") {
         const audioTomOne = new Audio("sounds/tom-1.mp3");
         audioTomOne.play();
-        break;
-
-        case "a":
+    } else if (key == "a") {
         const audioTomTwo = new Audio("sounds/tom-2.mp3");
         audioTomTwo.play();
-        break;
-
-        case "s":
+    } else if (key == "s") {
         const audioTomThree = new Audio("sounds/tom-3.mp3");
         audioTomThree.play();
-        break;
-
-        case "d":
+    } else if (key == "d") {
         const audioTomFour = new Audio("sounds/tom-4.mp3");
         audioTomFour.play();
-        break;
-
-        case "j":
+    } else if (key == "j") {
         const audioSnare = new Audio("sounds/snare.mp3");
         audioSnare.play();
-        break;
-
-        case "k":
+    } else if (key == "k") {
         const audioCrash = new Audio("sounds/crash.mp3");
         audioCrash.play();
-        break;
-
-        case "l":
+    } else if (key == "l") {
         const audioKick = new Audio("sounds/kick-bass.mp3");
         audioKick.play();
-        break;
-
-        default: console.log(key);
+    } else {
+        console.log('Invalid key!')
     }
 }
