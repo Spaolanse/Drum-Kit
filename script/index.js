c0bda86afc3ff1dfaaea087749fd6c8f0b5a89f7
Spaolanse <gustavospaolanse@gmail.com>
2024-06-21 16:31:59 -0300

    Update index.js

--- script/index.js
+++ script/index.js
@@ -23,28 +23,36 @@ document.addEventListener("keypress", function(event){
 });
 
 function sound(key) {
-    if (key == "w" || key == "W") {
-        const audioTomOne = new Audio("sounds/tom-1.mp3");
-        audioTomOne.play();
-    } else if (key == "a" || key == "A") {
-        const audioTomTwo = new Audio("sounds/tom-2.mp3");
-        audioTomTwo.play();
-    } else if (key == "s" || key == "S") {
-        const audioTomThree = new Audio("sounds/tom-3.mp3");
-        audioTomThree.play();
-    } else if (key == "d" || key == "D") {
-        const audioTomFour = new Audio("sounds/tom-4.mp3");
-        audioTomFour.play();
-    } else if (key == "j" || key == "J") {
-        const audioSnare = new Audio("sounds/snare.mp3");
-        audioSnare.play();
-    } else if (key == "k" || key == "K") {
-        const audioCrash = new Audio("sounds/crash.mp3");
-        audioCrash.play();
-    } else if (key == "l" || key == "L") {
-        const audioKick = new Audio("sounds/kick-bass.mp3");
-        audioKick.play();
-    } else {
-        console.log('Invalid key!')
+    switch(key) {
+        case "w": case "W":
+            const audioTomOne = new Audio("sounds/tom-1.mp3");
+            audioTomOne.play();
+            break;
+        case "a": case "A":
+            const audioTomTwo = new Audio("sounds/tom-2.mp3");
+            audioTomTwo.play();
+            break;
+        case "s": case "S":
+            const audioTomThree = new Audio("sounds/tom-3.mp3");
+            audioTomThree.play();
+            break;
+        case "d": case "D":
+            const audioTomFour = new Audio("sounds/tom-4.mp3");
+            audioTomFour.play();
+            break;
+        case "j": case "J":
+            const audioSnare = new Audio("sounds/snare.mp3");
+            audioSnare.play();
+            break;
+        case "k": case "K":
+            const audioCrash = new Audio("sounds/crash.mp3");
+            audioCrash.play();
+            break;
+        case "l": case "L":
+            const audioKick = new Audio("sounds/kick-bass.mp3");
+            audioKick.play();
+            break;
+        default:
+            console.log('Invalid key!');
     }
 }
